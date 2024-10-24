68f8c6600f13fe9e435377a3015a2105730e2f5d
jaynopponep <noppone.p@gmail.com>
2024-10-23 22:54:39 -0400

    adding basic configs

---
+++ nvim/init.lua
@@ -0,0 +1,1 @@
+require("jaynopponep.remap")
---
+++ nvim/lua/jaynopponep/remap.lua
@@ -0,0 +1,3 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- typing " pv" or space then pv in NORMAL MODE will lead you to netrw for config
