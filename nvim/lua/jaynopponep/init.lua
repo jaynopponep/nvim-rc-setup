d6143fef64c9ca477651766a47ac28d45d8c8bae
jaynopponep <noppone.p@gmail.com>
2024-10-24 22:06:59 -0400

    adding configs

--- nvim/lua/jaynopponep/init.lua
+++ nvim/lua/jaynopponep/init.lua
@@ -1,1 +1,7 @@
-print("hello world")
+vim.wo.relativenumber = true
+vim.wo.number = true
+vim.opt.backspace = { "indent", "eol", "start" }
+vim.opt.autoindent = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
