64ea7cb608fc4a0865ad59a98118a659db90cdb5
jaynopponep <noppone.p@gmail.com>
2026-02-23 09:49:59 -0500

    added neo-tree, catpuccin, diffview

--- nvim/lua/jaynopponep/init.lua
+++ nvim/lua/jaynopponep/init.lua
@@ -4,4 +4,8 @@ vim.opt.backspace = { "indent", "eol", "start" }
 vim.opt.autoindent = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
-vim.o.background = "dark"
+local appearance = vim.fn.system("defaults read -g AppleInterfaceStyle 2>/dev/null")
+vim.o.background = vim.trim(appearance) == "Dark" and "dark" or "light"
+vim.diagnostic.config({
+    virtual_text = true,
+})
--- nvim/lua/jaynopponep/lazy.lua
+++ nvim/lua/jaynopponep/lazy.lua
@@ -1,37 +1,45 @@
 -- this is typically where I should load my plugins including colorschemes
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-    if vim.v.shell_error ~= 0 then
-        vim.api.nvim_echo({
-            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out, "WarningMsg" },
-            { "\nPress any key to exit..." },
-        }, true, {})
-        vim.fn.getchar()
-        os.exit(1)
-    end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+vim.opt.termguicolors = true
 
 require("lazy").setup({
-    {
-        'nvim-telescope/telescope.nvim', 
-        tag = '0.1.8',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-	{ 
-		'ellisonleao/gruvbox.nvim',
-		name = 'gruvbox',
+	{
+		'nvim-telescope/telescope.nvim', 
+		tag = '0.1.8',
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
 		priority = 1000,
 		config = function()
-			require("gruvbox").setup({
+			require("catppuccin").setup({
+				flavour = "auto",
+				background = {
+					light = "latte",
+					dark = "macchiato",
+				},
+				integrations = {
+					neotree = true,
+				},
 			})
-			vim.o.background = "dark"
-			vim.cmd('colorscheme gruvbox')
+			vim.cmd('colorscheme catppuccin')
 		end
 	},
 	{
@@ -64,6 +72,37 @@ require("lazy").setup({
 			})
 		end,
 	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+		lazy = false,
+		config = function()
+			require("neo-tree").setup({
+				close_if_last_window = true,
+				filesystem = {
+					follow_current_file = { enabled = true },
+					use_libuv_file_watcher = true,
+				},
+			})
+			vim.keymap.set("n", "<leader>e", "<Cmd>Neotree toggle<CR>")
+		end,
+	},
+	{
+		"sindrets/diffview.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("diffview").setup()
+			vim.keymap.set("n", "<leader>gd", "<Cmd>DiffviewOpen<CR>")
+			vim.keymap.set("n", "<leader>gh", "<Cmd>DiffviewFileHistory %<CR>")
+			vim.keymap.set("n", "<leader>gc", "<Cmd>DiffviewClose<CR>")
+		end,
+	},
+	{ "MunifTanjim/nui.nvim" },
 	install = { colorscheme = { "habamax" } },
 	checker = { enabled = true },
 })
